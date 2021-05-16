66b5a56152598c5d64fb5769b4fe93810e5f1d86
Kevin Porras R <kevin702010@hotmail.com>
2021-05-16 14:47:15 -0600

    Added volume to mysql, download of yml and docker-compose

--- WPoverDockerInstall.sh
+++ WPoverDockerInstall.sh
@@ -1,3 +1,7 @@
+#wget -qO - https://raw.githubusercontent.com/Kevin7020/WordpressDockerInstall/main/WPoverDockerInstall.sh | bash
 sudo apt-get update
 sudo apt install -y docker.io docker-compose
 sudo usermod -aG docker $USER #Add the current user as part of the docker group
+ #sudo iptables -I INPUT -i eth0 -p tcp --dport 8080 -m comment --comment "# Wordpress Inbound #" -j ACCEPT # Open Port 8080 to the internet, after you reboot this is going to be lost !Use it only to test stuff!
+wget -q https://raw.githubusercontent.com/Kevin7020/WordpressDockerInstall/main/docker-compose.yml
+sudo docker-compose up -d
